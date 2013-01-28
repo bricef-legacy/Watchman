5611306a823bcf51fff4b6f6e01dae81dd38e4fd
Brice Fernandes <bfer@cbnl.com>
2013-01-28 17:50:03 +0000

    WIP

--- test/test_core.py
+++ test/test_core.py
@@ -6,12 +6,22 @@ import subprocess
 class TestCoreProcessSetup(unittest.TestCase):
   
   def test_process_from_config(self):
-    p = Process("test/date-forever.sh")
+    p = Process("test", "test/date-forever.sh")
     self.assertEqual(p.command, "test/date-forever.sh")
 
-  def test_process_has_proc(self):
-    p = Process({"run": "test/date-forever.sh"})
-    self.assertIsInstance(p, subprocess.Popen)
+  def test_process_not_started(self):
+    p = Process("test", "test/date-forever.sh")
+    self.assertEqual(p.pid, None)
+  
+  def test_start_and_stop_process(self):
+    p = Process("test", "test/date-forever.sh")
+    p.start()
+    self.assertTrue(p.is_running())
+    p.stop()
+    self.assertFalse(p.is_running())
+
+
+    
 
 class TestCoreWatchmanSetup(unittest.TestCase):
 
@@ -21,8 +31,10 @@ class TestCoreWatchmanSetup(unittest.TestCase):
   def test_watchman_init_none(self):
     # watchman initialised with no processes 
     self.assertEqual(len(self.wm.processes),0)
+  
+  def test_process_addition(self):
+    self.wm.add(Process("test", "test/date-forever.sh"))
+    self.assertEqual(len(self.wm.processes), 1)
 
 
 
-if __name__ == "__main__":
-  unittest.main()
--- watchman/core.py
+++ watchman/core.py
@@ -1,28 +1,54 @@
 
 import subprocess
 import sys
+import signal
+import psutil
 
 ON_POSIX = 'posix' in sys.builtin_module_names
 
-class Process(subprocess.Popen):
-  def __init__(self, cmd, name=None, *args, **kwargs):
-     self.name = name
-     self.command = cmd
+signal.signal(signal.SIGCHLD, signal.SIG_IGN)
 
-     defaults = {
-         'stdout': subprocess.PIPE,
-         'stderr': subprocess.STDOUT,
-         'shell': True,
-         'bufsize': 1,
-         'close_fds': ON_POSIX
-     }
-     defaults.update(kwargs)
+class Process:
+  def __init__(self, name, cmd, *args, **kwargs):
+    self.popen = None
+    self.name = name
+    self.command = cmd
+ 
+    self.popen_defaults = {
+        'stdin': subprocess.PIPE,
+        'stdout': subprocess.PIPE,
+        'stderr': subprocess.STDOUT,
+        'shell': True,
+        'bufsize': 1,
+        'close_fds': ON_POSIX
+    }
+    self.popen_defaults.update(kwargs)
+    self.args = args
+ 
 
-     super(Process, self).__init__(cmd, *args, **defaults) 
+  def start(self):
+    print("Starting: "+self.command+" "+str(self.popen_defaults))
+    self.popen = psutil.Popen(self.command, **self.popen_defaults)
+  
+  @property
+  def pid(self):
+    if self.popen:
+      return self.popen.pid
 
+  def stop(self):
+    if self.popen:
+      self.popen.kill()
+  
+  def is_running(self):
+    if self.popen:
+      return self.popen.is_running()
+    else:
+      return False
 
 class Watchman:
   def __init__(self):
     self.processes = []
 
+  def add(self,process):
+    self.processes.append(process)
 
